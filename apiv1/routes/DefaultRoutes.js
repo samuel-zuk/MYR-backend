2630e0a3ddce16d9762cd544941169b74d327882
Jason <jason_kiesling@student.uml.edu>
2018-09-20 00:15:19 -0400

    Setting up CircleCI

--- apiv1/routes/DefaultRoutes.js
+++ apiv1/routes/DefaultRoutes.js
@@ -1,11 +1,5 @@
 let express = require('express');
 let router = express.Router();
-const rateLimit = require("express-rate-limit");
-
-const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,   // 15 minutes
-    max: 100                    // max number of requests
-});
 
 router.get('/', function (req, res, next) {
     res.status(404).send({
