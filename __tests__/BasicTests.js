2be43a84559a8ac86001beca138d5e02126b60ae
Jason <jason_kiesling@student.uml.edu>
2018-09-20 11:12:44 -0400

    Setting up CircleCI and Jest

--- __tests__/BasicTests.js
+++ __tests__/BasicTests.js
@@ -1,7 +1,14 @@
 const request = require('supertest');
 const app = require('./../app')
+
 describe('Test the root path', () => {
     test('It should response the GET method', () => {
         return request(app).get('/').expect(200);
+    });
+})
+
+describe('Test for failure', () => {
+    test('It should response the GET method', () => {
+        return request(app).get('/').expect(404);
     });
 })
