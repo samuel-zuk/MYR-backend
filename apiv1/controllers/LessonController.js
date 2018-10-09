cc6065884c96644178ddebc2018972a08debe269
Jason <jason_kiesling@student.uml.edu>
2018-10-09 15:22:31 -0400

    Assorted fixes

--- apiv1/controllers/LessonController.js
+++ apiv1/controllers/LessonController.js
@@ -249,7 +249,7 @@ module.exports = {
               error: err
             });
           }
-          return res.status(200).json();
+          return res.status(200).json("Lesson deleted");
         });
       }
     })
