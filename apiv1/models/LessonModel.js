4c5ba44afea97c96b04a8599e501b026c21b4cbe
Jason Kiesling <jason@learnmyr.org>
2019-08-03 15:08:19 -0400

    Removed lesson numbers from lessons

--- apiv1/models/LessonModel.js
+++ apiv1/models/LessonModel.js
@@ -2,13 +2,10 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let LessonSchema = new Schema({
-	'lessonNumber': Number,
 	'name': String,
 	'prompt': String,
 	'code': String,
-	'categories': Array,
-	'next': String,
-	'previous': String
+	'categories': Array
 });
 
 module.exports = mongoose.model('Lesson', LessonSchema);
