5669bb0ffa77a3c21a86e86ba39bc385ed7f9464
Jason Kiesling <jason@learnmyr.org>
2019-08-03 15:11:54 -0400

    Removed lesson numbers from lesson controller

--- apiv1/controllers/LessonController.js
+++ apiv1/controllers/LessonController.js
@@ -14,9 +14,6 @@ module.exports = {
   list: function (req, res) {
     // ToDo: Support comma separated list of categories
     let category = req.query.category ? { categories: req.query.category } : null;
-    let lessonNumber = req.query.lessonNumber ? { lessonNumber: req.query.lessonNumber } : null;
-    let previous = req.query.previous ? { previous: req.query.previous } : null;
-    let next = req.query.next ? { next: req.query.next } : null;
 
     let range;
     let pageSize;
@@ -38,7 +35,7 @@ module.exports = {
       };
     }
 
-    let queryParams = { ...category, ...lessonNumber, ...previous, ...next };
+    let queryParams = { ...category };
 
     LessonModel.find(queryParams, {}, filter, function (err, Lesson) {
       if (err) {
@@ -84,27 +81,6 @@ module.exports = {
   },
 
   /**
-   * LessonController.show_via_lessonNumber()
-   */
-  show_via_lessonNumber: function (req, res) {
-    let lessonNumber = req.params.lessonNumber;
-    LessonModel.findOne({ lessonNumber: lessonNumber }, function (err, Lesson) {
-      if (err) {
-        return res.status(500).json({
-          message: 'Error when getting Lesson.',
-          error: err
-        });
-      }
-      if (!Lesson) {
-        return res.status(404).json({
-          message: 'No such Lesson'
-        });
-      }
-      return res.json(Lesson);
-    });
-  },
-
-  /**
     * LessonController.create()
     */
   /*
@@ -112,14 +88,10 @@ module.exports = {
    */
   create: function (req, res) {
     let Lesson = new LessonModel({
-      lessonNumber: req.body.lessonNumber,
       name: req.body.name,
       prompt: req.body.prompt,
       code: req.body.code,
       categories: req.body.categories,
-      next: req.body.next,
-      previous: req.body.previous
-
     });
     let token = req.headers['x-access-token'];
 
@@ -146,7 +118,6 @@ module.exports = {
    */
   update: function (req, res) {
     let token = req.headers['x-access-token'];
-
     verify.isAdmin(token).then(function (answer) {
       if (!answer) {
         res.status(401).send('Error 401: Not authorized');
@@ -166,14 +137,10 @@ module.exports = {
             });
           }
 
-
-          Lesson.lessonNumber = req.body.lessonNumber ? req.body.lessonNumber : Lesson.lessonNumber;
           Lesson.name = req.body.name ? req.body.name : Lesson.name;
           Lesson.prompt = req.body.prompt ? req.body.prompt : Lesson.prompt;
           Lesson.code = req.body.code ? req.body.code : Lesson.code;
           Lesson.categories = req.body.categories ? req.body.categories : Lesson.categories;
-          Lesson.next = req.body.next ? req.body.next : Lesson.next;
-          Lesson.previous = req.body.previous ? req.body.previous : Lesson.previous;
 
           Lesson.save(function (err, Lesson) {
             if (err) {
@@ -188,57 +155,6 @@ module.exports = {
         });
       }
     });
-  },
-
-  /**
-   * LessonController.update_via_lessonNumber()
-   */
-  update_via_lessonNumber: function (req, res) {
-    let token = req.headers['x-access-token'];
-
-    verify.isAdmin(token).then(function (answer) {
-      if (!answer) {
-        res.status(401).send('Error 401: Not authorized');
-      }
-      else {
-        let lessonNumber = req.params.lessonNumber;
-        LessonModel.findOne({ lessonNumber: lessonNumber }, function (err, Lesson) {
-          if (err) {
-            return res.status(500).json({
-              message: 'Error when getting Lesson',
-              error: err
-            });
-          }
-          if (!Lesson) {
-            return res.status(404).json({
-              message: 'No such Lesson'
-            });
-          }
-
-          Lesson.lessonNumber = req.body.lessonNumber ? req.body.lessonNumber : Lesson.lessonNumber;
-          Lesson.name = req.body.name ? req.body.name : Lesson.name;
-          Lesson.prompt = req.body.prompt ? req.body.prompt : Lesson.prompt;
-          Lesson.code = req.body.code ? req.body.code : Lesson.code;
-          Lesson.categories = req.body.categories ? req.body.categories : Lesson.categories;
-          Lesson.next = req.body.next ? req.body.next : Lesson.next;
-          Lesson.previous = req.body.previous ? req.body.previous : Lesson.previous;
-
-          Lesson.save(function (err, Lesson) {
-            if (err) {
-              return res.status(500).json({
-                message: 'Error when updating Lesson.',
-                error: err
-              });
-            }
-
-            return res.json(Lesson);
-          });
-        });
-      }
-    });
-
-
-
   },
 
   /**
@@ -261,32 +177,6 @@ module.exports = {
             });
           }
           return res.status(200).json(Lesson);
-        });
-      }
-    });
-
-  },
-
-  /**
-   * LessonController.remove_via_lessonNumber()
-   */
-  remove_via_lessonNumber: function (req, res) {
-    let token = req.headers['x-access-token'];
-
-    verify.isAdmin(token).then(function (answer) {
-      if (!answer) {
-        res.status(401).send('Error 401: Not authorized');
-      }
-      else {
-        let lessonNumber = req.params.lessonNumber;
-        LessonModel.deleteOne({ lessonNumber: lessonNumber }, function (err, Lesson) {
-          if (err) {
-            return res.status(500).json({
-              message: 'Error when deleting the Lesson.',
-              error: err
-            });
-          }
-          return res.status(204).json(Lesson);
         });
       }
     });
