a0a3d14a2c4c71bedf27e5cbaf4c580dc0fa5105
Keith Valin <keith@mailtux.net>
2019-10-11 12:04:20 -0400

    Removed Lesson API endpoint, updated Courses Model to include lessons

--- apiv1/models/CourseModel.js
+++ apiv1/models/CourseModel.js
@@ -1,12 +1,18 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
+let LessonSchema = new Schema({
+	'name': { type: String, required: true },
+	'prompt': String,
+	'code': { type: String, required: true }
+});
+
 let CourseSchema = new Schema({
     'shortname': String,
     'name': String,
     'difficulty': Number,
     'description': String,
-    'lessons': Array
+    'lessons': [LessonSchema]
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
