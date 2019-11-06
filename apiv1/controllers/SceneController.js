7f058d470c8e7e58cb771987551815ec3d682a9a
Keith Valin <keith@mailtux.net>
2019-11-06 14:55:00 -0500

    Refactored a few bits of create, updated delete to require a uid, and delete now will only delete scenes owned by that uid

--- apiv1/controllers/SceneController.js
+++ apiv1/controllers/SceneController.js
@@ -2,36 +2,38 @@ let verify = require('../authorization/verifyAuth.js');
 let SceneSchema = require('../models/SceneModel');
 let mongoose = require('mongoose');
 
+function buildScene(body, settings){
+    return new SceneSchema({
+        name: body.name,
+        uid: body.uid,
+        code: body.code,
+        settings:{
+            map: {
+                skyColor: settings.map.skyColor,
+                showFloor: settings.map.showFloor,
+                floorColor: settings.map.floorColor,
+                showCoordHelper: settings.map.showCoordHelper
+            },
+            camera: {
+                position: settings.camera.position,
+                config: (settings.camera.config ? settings.camera.config : 0),
+                canFly: settings.camera.canFly
+            },
+            viewOnly: settings.viewOnly,
+            collection: settings.collection
+        },
+        createTime: new Date(),
+        updateTime: new Date()
+    });
+}
+
 module.exports = {
     create: function(req, res){
         let body = req.body;
-        if(Object.keys(body).length === 0){ //Check if a body was supplied
-            res.status(400).send("Bad Request");
-            return;
+        if(Object.keys(body).length === 0 && body.uid === ""){ //Check if a body was supplied
+            return res.status(400).send("Bad Request");
         }
-        let settings = body.settings;
-        let newScene = new SceneSchema({
-            name: body.name,
-            uid: body.uid,
-            code: body.code,
-            settings:{
-                map: {
-                    skyColor: settings.map.skyColor,
-                    showFloor: settings.map.showFloor,
-                    floorColor: settings.map.floorColor,
-                    showCoordHelper: settings.map.showCoordHelper
-                },
-                camera: {
-                    position: settings.camera.position,
-                    config: (settings.camera.config ? settings.camera.config : 0),
-                    canFly: settings.camera.canFly
-                },
-                viewOnly: settings.viewOnly,
-                collection: settings.collection
-            },
-            createTime: new Date(),
-            updateTime: new Date()
-        });
+        let newScene = buildScene(body, body.settings);
         newScene.save(function (err, result){
             if(err){
                 return res.status(500).json({
@@ -44,7 +46,18 @@ module.exports = {
     },
     delete: function (req, resp){
         let id = req.params.id;
-        SceneSchema.findByIdAndRemove(id, function(err, scene){
+
+        if(!req.headers['x-access-token']){
+            return resp.status(400).json({
+                message: "Missing user ID",
+                error: "Bad Request"
+            });
+        }
+
+        let uid = req.headers['x-access-token'];
+
+        //Check to make sure that the scene exists and is owned by the uid before removing it
+        SceneSchema.findById(id, function(err, scene){
             if(err){
                 return resp.status(500).json({
                     message: "Error deleting Scene",
@@ -57,7 +70,23 @@ module.exports = {
                     error: "Scene Not Found"
                 });
             }
-            return resp.send(204);
+            if(scene.uid != uid){
+                return resp.status(401).json({
+                    message: `You do not own scene "${id}"`,
+                    error: "Unauthorized"
+                });
+            }
+            else{
+                SceneSchema.findByIdAndRemove(id, function(err, scene){
+                    if(err){
+                        return resp.status(500).json({
+                            message: "Error deleting Scene",
+                            error: err
+                        });
+                    }
+                    return resp.send(204); //No Content
+                });
+            }
         });
     }
 };
