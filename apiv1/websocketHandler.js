32768c051e228bd185b797484cc29137ff3b9e2a
Keith Valin <keith@mailtux.net>
2020-06-01 09:38:50 -0400

    Added test database for collections.
    Moved websocket functionality to /apiv1/websocketController for better
    separation

--- apiv1/websocketHandler.js
+++ apiv1/websocketHandler.js
@@ -1,36 +1,34 @@
 let Conns = require("./models/ConnectionModel");
 const ObjectID = require("mongoose").Types.ObjectId;
-/**
- * 
- * @param {SocketIO.Socket} socket 
- */
-class SocketHandler {
-    /**
-     * 
-     * @param {SocketIO.Socket} sockets
-     */
-    constructor(socket){
-        this.socket = socket;
-        this.id = socket.id;
-        socket.on("scene", this.handleSceneEvent);
-        socket.on("disconnect", this.handleDisconnect);
-    }
-    
-    async handleDisconnect(){       
+
+function setupSockets(sockets) {
+    sockets.on("connection", (socket) => {
+        handleDisconnect(socket);
+        handleSceneEvent(socket);
+        handleSaveEvent(sockets.sockets, socket);
+    });
+    Conns.deleteMany({}).catch((err) => {
+        console.error(`Error cleaning connection database: ${err}`);
+    });
+}
+
+function handleDisconnect(socket) {
+    socket.on("disconnect", async () => {
         try{
             await Conns.updateOne({
-                clientIDs: this.id
+                clientIDs: socket.id
             }, {
                 "$pull": {
-                    clientIDs: this.id
+                    clientIDs: socket.id
                 }
             });
         }catch(err){
-            console.error(`Could not remove connection ${this.id} from the database: ${err}`);
+            console.error(`Could not remove connection ${socket.id} from the database: ${err}`);
         }
-    }
-    
-    async handleSceneEvent(sceneID){
+    });
+}
+function handleSceneEvent(socket) {
+    socket.on("scene", async (sceneID) => {
         if(typeof(sceneID) !== "string"){
             return;
         }
@@ -47,7 +45,7 @@ class SocketHandler {
                 await Conns.create({
                     sceneID: ObjectID(sceneID),
                     clientIDs: [
-                        this.id
+                        socket.id
                     ]
                 });
             }catch(err){
@@ -59,14 +57,30 @@ class SocketHandler {
         try{
             await connDoc.update({
                 "$push": {
-                    "clientIDs": this.id
+                    "clientIDs": socket.id
                 }
             });
         }catch(err){
             socket.emit("error", err);
         }
         return;
-    }
+    });
+}
+function handleSaveEvent(socketList, socket) {
+    socket.on("save", async () => {
+        let conns;
+        try{
+            conns = await Conns.findOne({"clientIDs": socket.id});
+        }catch(err){
+            socket.emit("error", err);
+        }
+        conns.clientIDs.forEach(id => {
+            if(id !== socket.id){
+                console.log(`Updating ${socket.id}`);
+                socketList[id].emit("update");
+            }
+        });
+    });
 }
 
-module.exports = SocketHandler;
+module.exports = setupSockets;
