9dd91bdd13b3c7bb50af456a9018c91982bc4e6a
jlu18 <justin_lu@student.uml.edu>
2021-02-12 13:55:40 -0500

    Add settings in lesson schema

--- apiv1/models/CourseModel.js
+++ apiv1/models/CourseModel.js
@@ -4,7 +4,17 @@ let Schema = mongoose.Schema;
 let LessonSchema = new Schema({
 	'name': { type: String, required: true },
 	'prompt': String,
-	'code': { type: String, required: true }
+	'code': { type: String, required: true },
+    'settings': {
+        'skyColor': String,
+        'showFloor': Boolean,
+        'floorColor': String,
+        'showCoordHelper': Boolean,
+        'camPosition': String,
+        'camConfig': Number,
+        'canFly': Boolean,
+        'viewOnly': Boolean
+    },
 }, {_id : false});
 
 let CourseSchema = new Schema({
